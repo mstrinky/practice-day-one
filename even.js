85a66fa8c479bbbde1a2efcb83a118fd8ded61f9
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-11 20:31:37 +0600

    firstcharacter

--- even.js
+++ even.js
@@ -1,3 +1,4 @@
+// Write a function that takes in a number and returns true if the number is even, and false otherwise. 
 function isEven(number){
     if(number%2 === 0){
         return true;
---
+++ firstCharacter.js
@@ -0,0 +1,4 @@
+// Write a function that takes in a string and returns the first character of the string. 
+var clgName = 'Tpi';
+var firstLetter = clgName.charAt(0);
+console.log(firstLetter);
