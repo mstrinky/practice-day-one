b16088d0b2ac4843e03131a4dfa1a7ea69ea47d9
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-11 19:43:58 +0600

    largest number

--- problem-solving.js
+++ problem-solving.js
@@ -1,11 +1,23 @@
-// problem number 1: 
-function numbers(a,b){
-    return a+b;
-}
-var additon = numbers(2,3);
-console.log(additon);
+// // problem number 1: 
+// function numbers(a,b){
+//     return a+b;
+// }
+// var additon = numbers(2,3);
+// console.log(additon);
 
-// problem number 2: 
-var myName = 'mstrinkyakter';
-var length = myName.length;
-console.log(length);
+// // problem number 2: 
+// var myName = 'mstrinkyakter';
+// var length = myName.length;
+// console.log(length);
+
+// problem number 3: 
+var numbers = [1,2,3,4,5,6];
+var largestNumber =numbers [0];
+for( let i=0; i<numbers.length; i++){
+if(largestNumber < numbers[i]){
+    largestNumber = numbers[i];
+}
+}
+console.log(largestNumber);
+
+
