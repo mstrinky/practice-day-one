9dcb27c784b928afbba7ac7f796716980d454fda
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-11 20:16:38 +0600

    uppercase

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
+// problem number 6:
 var bangla = 60;
 var english = 50;
 var math = 80;
 var total = bangla + english + math;
 var average = total/3;
 console.log(average);
+
---
+++ uppercase.js
@@ -0,0 +1,3 @@
+var myName = 'my name is riky';
+var uppercase = hudai.toUpperCase();
+console.log(uppercase);
