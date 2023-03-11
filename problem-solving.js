3f8d7a02d83132816ce0d05d820895120458b424
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-11 20:00:45 +0600

    problem 4

--- problem-solving.js
+++ problem-solving.js
@@ -1,14 +1,14 @@
-// // problem number 1: 
-// function numbers(a,b){
-//     return a+b;
-// }
-// var additon = numbers(2,3);
-// console.log(additon);
+// problem number 1: 
+function numbers(a,b){
+    return a+b;
+}
+var additon = numbers(2,3);
+console.log(additon);
 
-// // problem number 2: 
-// var myName = 'mstrinkyakter';
-// var length = myName.length;
-// console.log(length);
+// problem number 2: 
+var myName = 'mstrinkyakter';
+var length = myName.length;
+console.log(length);
 
 // problem number 3: 
 var numbers = [1,2,3,4,5,6];
---
+++ true-false.js
@@ -0,0 +1,12 @@
+// problem number 4: 
+
+    var fruitName = 'mango';
+    var juiceName = 'mangojuice';
+    if(fruitName === juiceName){
+        console.log(true);
+    }
+    else{
+       console.log(false);
+    }
+   
+
