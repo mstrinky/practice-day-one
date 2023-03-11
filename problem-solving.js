2f9865fccf90d5f96e5bbf7880d3b19523241179
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-11 19:12:32 +0600

    length of the string

--- problem-solving.js
+++ problem-solving.js
@@ -1,5 +1,11 @@
+// problem number 1: 
 function numbers(a,b){
     return a+b;
 }
 var additon = numbers(2,3);
 console.log(additon);
+
+// problem number 2: 
+var myName = 'mstrinkyakter';
+var length = myName.length;
+console.log(length);
